60ea65e1e6f81d858676a68aefc2df82ee0b4f19
Ricardo-509 <pierrericardo050@gmail.com>
2025-09-21 18:00:41 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;KN8hzBSL#1f8p5wMe1Ads-JAyhEm04YBaterJYESvSnCPWn9WGUU", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "50947646367", // Put Owner Number Here 
+  PREFIX: "●", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
 
